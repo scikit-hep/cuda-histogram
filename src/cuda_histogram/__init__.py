b01cdea8a00a0ea80771c001bae89ea926f94be7
Saransh <saransh0701@gmail.com>
2024-07-22 22:36:25 +0530

    Users should use the plotting libraries to plot hists on CPU

--- src/cuda_histogram/__init__.py
+++ src/cuda_histogram/__init__.py
@@ -9,30 +9,14 @@ The package follows `UHI <https://uhi.readthedocs.io>`__ and keeps its API simil
 from __future__ import annotations
 
 from cuda_histogram.hist_tools import Bin, Cat, Hist, Interval, StringBin
-from cuda_histogram.plot import (
-    clopper_pearson_interval,
-    normal_interval,
-    plot1d,
-    plot2d,
-    plotgrid,
-    plotratio,
-    poisson_interval,
-)
 
 from ._version import version as __version__
 
-__all__ = [
+__all__: list[str] = [
     "Hist",
     "Bin",
     "Interval",
     "Cat",
     "StringBin",
-    "poisson_interval",
-    "clopper_pearson_interval",
-    "normal_interval",
-    "plot1d",
-    "plotratio",
-    "plot2d",
-    "plotgrid",
     "__version__",
 ]
